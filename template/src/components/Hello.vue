fb5640a1096a81f3cc877469065243d65c6d3c74
Ralph Pillichshammer <r.pillichshammer@neonblack.at>
2017-08-07 15:10:24 +0200

    remove space to match lint config

--- template/src/components/Hello.vue
+++ template/src/components/Hello.vue
@@ -23,7 +23,7 @@
 <script>
 export default {
   name: 'hello',
-  data{{#unless_eq lintConfig "airbnb"}} {{/unless_eq}}() {
+  data{{#unless_eq lintConfig "airbnb"}}{{#unless_eq lintConfig "neonblack"}} {{/unless_eq}}{{/unless_eq}}() {
     return {
       msg: 'Welcome to Your Vue.js App'{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
     }{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
