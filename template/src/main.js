0f05cc4133cd7269f16633251e982cd2e5c9c694
Ralph Pillichshammer <r.pillichshammer@neonblack.at>
2017-08-07 15:21:55 +0200

    name root component after app

--- template/src/main.js
+++ template/src/main.js
@@ -16,11 +16,5 @@ new Vue({
   {{#router}}
   router,
   {{/router}}
-  {{#if_eq build "runtime"}}
-  render: h => h(App){{#if_eq lintConfig "airbnb"}},{{/if_eq}}
-  {{/if_eq}}
-  {{#if_eq build "standalone"}}
-  template: '<App/>',
-  components: { App }{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
-  {{/if_eq}}
+  ...App
 }){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
